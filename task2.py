735264a40b67ebc6fcf5c01e13ca8a3aa03dd8c4
Insaf Gazetdinov <81470111+bouncemew@users.noreply.github.com>
2022-09-15 15:21:27 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
-num = int(input("Введите число: "))
+num = input("Введите число: ")
 sum = 0
-while num!=0:
-    sum = sum + num % 10
-    num = num // 10
+for char in num:
+    char = int(char)
+    sum += char
 print(sum)
