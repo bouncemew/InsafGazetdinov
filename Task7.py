6bd6a37929b2a837f55e528fa1604b14317578ad
Insaf Gazetdinov <81470111+bouncemew@users.noreply.github.com>
2022-11-17 14:28:52 +0300

    Update Task7.py

--- Task7.py
+++ Task7.py
@@ -4,9 +4,9 @@ class Time():
 
     @classmethod
     def from_string(cls, timestr='23:59:50'):
-        time_re = re.compile('^(?:([01]?\d|2[0-3]):([0-5]?\d):)?([0-5]?\d)$')
-        new_time = re.match(time_re, timestr)
-        print(new_time.group())
+        cls.time_re = re.compile('^(?:([01]?\d|2[0-3]):([0-5]?\d):)?([0-5]?\d)$')
+        cls.new_time = re.match(cls.time_re, timestr)
+        print(cls.new_time.group())
 
 t = Time()
 t.from_string()
